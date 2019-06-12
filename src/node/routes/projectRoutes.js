a397bc1aa8a35978afe222f5381fdec7aa0a259a
Scott Lively <scott.lively@gmail.com>
2019-06-11 20:54:30 -0700

    hide un-used routes for now

--- src/node/routes/projectRoutes.js
+++ src/node/routes/projectRoutes.js
@@ -4,13 +4,13 @@ const { authenticate } = require('../middleware/ecan-passport-strategy');
 const { roleValidation } = require('../lib/roles');
 
 module.exports = (app) => {
-    app.get(routes.GET_PROJECTS, authenticate, getAll);
+    // app.get(routes.GET_PROJECTS, authenticate, getAll);
 
-    app.get(routes.GET_PROJECT_BY_ID, authenticate, getByID);
+    // app.get(routes.GET_PROJECT_BY_ID, authenticate, getByID);
 
-    app.get(routes.GET_ORG_PROJECTS_BY_ID, authenticate, getMultipleById);
-    
-    app.post(routes.POST_PROJECT, authenticate, /*roleValidation,*/ createOrUpdate);
+    // app.get(routes.GET_ORG_PROJECTS_BY_ID, authenticate, getMultipleById);
 
-    app.delete(routes.DELETE_PROJECT, authenticate, deleteProject);
+    // app.post(routes.POST_PROJECT, authenticate, /*roleValidation,*/ createOrUpdate);
+
+    // app.delete(routes.DELETE_PROJECT, authenticate, deleteProject);
 };
--- src/node/routes/userRoutes.js
+++ src/node/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const routes = require('../config/routes');
 const { getAll, getByID, getMultipleById, createOrUpdate, deleteUser, login, postUserRole, deleteUserRole } = require('../services/userService');
 const { authenticate } = require('../middleware/ecan-passport-strategy');
-const { addRolePermission } = require('../lib/roles');
+//const { addRolePermission } = require('../lib/roles');
 
 module.exports = function (app) {
     app.get(routes.GET_USERS, authenticate, getAll);
@@ -12,15 +12,15 @@ module.exports = function (app) {
 
     app.post(routes.LOGIN, login);
 
-    app.post(routes.POST_USER, createOrUpdate);
+    // app.post(routes.POST_USER, createOrUpdate);
 
-    app.post(routes.UPDATE_USER, createOrUpdate);
+    // app.post(routes.UPDATE_USER, createOrUpdate);
 
-    app.post(routes.CREATE_USER_ROLE, authenticate, addRolePermission, postUserRole);
+    // app.post(routes.CREATE_USER_ROLE, authenticate, addRolePermission, postUserRole);
 
-    app.delete(routes.DELETE_USER_ROLE, authenticate, deleteUserRole);
+    // app.delete(routes.DELETE_USER_ROLE, authenticate, deleteUserRole);
 
-    app.delete(routes.DELETE_USER, authenticate, deleteUser);
+    // app.delete(routes.DELETE_USER, authenticate, deleteUser);
     /*
         Todo: I think a helpful route would be a route that returns a users roles.
     */
